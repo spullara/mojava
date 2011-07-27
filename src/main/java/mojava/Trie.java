c459c34ebc0c1871eeb1cad4a0926784a3436761
spullara <spullara@yahoo.com>
2011-07-26 18:54:49 -0700

    fixes to the trie

--- src/main/java/mojava/Trie.java
+++ src/main/java/mojava/Trie.java
@@ -6,22 +6,22 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * TODO: Edit this
+ * Store values using a list of keys. Look them up by a list of keys or grab all below a certain prefix key.
  * <p/>
  * User: sam
  * Date: 7/16/11
  * Time: 11:23 AM
  */
-public class Trie<K extends List, V> {
-  private Map<Object, Trie<K, V>> root = new HashMap<Object, Trie<K, V>>();
+public class Trie<K, V> {
+  private Map<K, Trie<K, V>> root = new HashMap<K, Trie<K, V>>();
   private V value;
 
-  public void put(List list, V value) {
+  public void put(List<K> list, V value) {
     if (list.size() == 0) {
       this.value = value;
       return;
     }
-    Object key = list.get(0);
+    K key = list.get(0);
     Trie<K, V> next = root.get(key);
     if (next == null) {
       next = new Trie<K, V>();
@@ -30,18 +30,18 @@ public class Trie<K extends List, V> {
     next.put(list.subList(1, list.size()), value);
   }
 
-  public V get(List list) {
+  public V get(List<K> list) {
     if (list.size() == 0) {
       return value;
     }
     return root.get(list.get(0)).get(list.subList(1, list.size()));
   }
 
-  public List<V> getAll(List list) {
+  public List<V> getAll(List<K> list) {
     List<V> values = new ArrayList<V>();
-    if (list.size() == 0 && value != null) values.add(value);
     if (list.size() == 0) {
-      for (Map.Entry<Object, Trie<K, V>> entry : root.entrySet()) {
+      if (value != null) values.add(value);
+      for (Map.Entry<K, Trie<K, V>> entry : root.entrySet()) {
         values.addAll(entry.getValue().getAll(list));
       }
     } else {
--- src/test/java/mojava/TrieTest.java
+++ src/test/java/mojava/TrieTest.java
@@ -3,7 +3,6 @@ package mojava;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -17,12 +16,13 @@ import static junit.framework.Assert.assertEquals;
 public class TrieTest {
   @Test
   public void full() {
-    Trie<List<String>, String> trie = new Trie<List<String>, String>();
+    Trie<String, String> trie = new Trie<String, String>();
     trie.put(Arrays.asList("sam", "pullara", "test"), "sam pullara test");
+    trie.put(Arrays.asList("sam", "pullara"), "sam pullara");
     trie.put(Arrays.asList("sam", "pullara", "test2"), "sam pullara test2");
     trie.put(Arrays.asList("sam", "pullara3", "test"), "sam pullara3 test");
     trie.put(Arrays.asList("anna", "pullara3", "test"), "anna pullara3 test");
     assertEquals("sam pullara test", trie.get(Arrays.asList("sam", "pullara", "test")));
-    assertEquals(Arrays.asList("sam pullara test", "sam pullara test2"), trie.getAll(Arrays.asList("sam", "pullara")));
+    assertEquals(Arrays.asList("sam pullara", "sam pullara test", "sam pullara test2"), trie.getAll(Arrays.asList("sam", "pullara")));
   }
 }
